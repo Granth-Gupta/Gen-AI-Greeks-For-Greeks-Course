904a8b93838af11875549d6be83606fdad6699c2
GRANTH GUPTA <102872537+Granth-Gupta@users.noreply.github.com>
2026-09-05 18:53:50 +0530

    Project with local SLM

--- Practice/practice.py
+++ Practice/practice.py
@@ -1,6 +1,15 @@
-# Combination of boolean expressions
+# Source - https://stackoverflow.com/q/52224813
+# Posted by Eun Jee Lee, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-09-05, License - CC BY-SA 4.0
 
-a = True
-b = False
+import warnings
+warnings.filterwarnings(action='ignore')
 
-print(a and b)
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import make_regression
+
+X, y = make_regression(n_features=4, n_informative=2, random_state=0, shuffle=False)
+regr = RandomForestRegressor(max_depth=2, random_state=0)
+regr.fit(X, y)
+print(regr.predict([[0, 0, 0, 0]]))
+
